971a258f57f575945facbbc3a2e278ecec5b8fc9
ElAxolote <106245108+axoloteSupremo@users.noreply.github.com>
2022-06-04 17:04:45 -0500

    Add files via upload

---
+++ Sum_Difference.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std; 
+int main(){
+    float x = 123.456F, //or double
+          y = 76.543F,
+          sum; 
+    sum = x + y; 
+    cout << "Total:        "
+         << x << "+" << y << "=" << sum << endl;
+    cout <<"Difference: "
+         << x << "-" << y << "=" << (x-y) << endl;
+    return 0; 
+}
